4484ec36decb7450fc216935a25478afce1f49c7
Leonard Dahlmann <leo.dahlmann@gmail.com>
2019-01-15 22:02:32 +0100

    Add AiSandbox project

---
+++ AiSandbox/Program.cs
@@ -0,0 +1,133 @@
+﻿#region copyright
+// SabberStone, Hearthstone Simulator in C# .NET Core
+// Copyright (C) 2017-2019 SabberStone Team, darkfriend77 & rnilva
+//
+// SabberStone is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License.
+// SabberStone is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using SabberStoneCore.Config;
+using SabberStoneCore.Enums;
+using SabberStoneCore.Model;
+using SabberStoneCore.Tasks;
+using SabberStoneCore.Tasks.PlayerTasks;
+
+namespace SabberStoneCoreAi
+{
+	internal class Program
+	{
+		private static readonly Random Rnd = new Random();
+
+		private static void Main(string[] args)
+		{
+			Console.WriteLine("Starting test setup.");
+
+			RandomGames();
+
+			Console.WriteLine("Test end!");
+			Console.ReadLine();
+		}
+
+		public static GameConfig GameConfig()
+		{
+			var deck = new List<Card>()
+						{
+						Cards.FromName("Blessing of Might"),
+						Cards.FromName("Blessing of Might"),
+						Cards.FromName("Gnomish Inventor"),
+						Cards.FromName("Gnomish Inventor"),
+						Cards.FromName("Goldshire Footman"),
+						Cards.FromName("Goldshire Footman"),
+						Cards.FromName("Hammer of Wrath"),
+						Cards.FromName("Hammer of Wrath"),
+						Cards.FromName("Hand of Protection"),
+						Cards.FromName("Hand of Protection"),
+						Cards.FromName("Holy Light"),
+						Cards.FromName("Holy Light"),
+						Cards.FromName("Ironforge Rifleman"),
+						Cards.FromName("Ironforge Rifleman"),
+						Cards.FromName("Light's Justice"),
+						Cards.FromName("Light's Justice"),
+						Cards.FromName("Lord of the Arena"),
+						Cards.FromName("Lord of the Arena"),
+						Cards.FromName("Nightblade"),
+						Cards.FromName("Nightblade"),
+						Cards.FromName("Raid Leader"),
+						Cards.FromName("Raid Leader"),
+						Cards.FromName("Stonetusk Boar"),
+						Cards.FromName("Stonetusk Boar"),
+						Cards.FromName("Stormpike Commando"),
+						Cards.FromName("Stormpike Commando"),
+						Cards.FromName("Stormwind Champion"),
+						Cards.FromName("Stormwind Champion"),
+						Cards.FromName("Stormwind Knight"),
+						Cards.FromName("Stormwind Knight")
+						};
+			var gameConfig = new GameConfig()
+			{
+				StartPlayer = -1,
+				Player1Name = "Lars",
+				Player1HeroClass = CardClass.PALADIN,
+				Player1Deck = deck,
+				Player2Name = "Leo",
+				Player2HeroClass = CardClass.PALADIN,
+				Player2Deck = deck,
+				FillDecks = false,
+				Shuffle = true,
+				SkipMulligan = false,
+				Logging = false,
+				History = false
+			};
+			return gameConfig;
+		}
+
+		public static void RandomGames()
+		{
+			int total = 10000;
+			var watch = Stopwatch.StartNew();
+
+			long turns = 0;
+			int[] wins = new[] { 0, 0 };
+			for (int i = 0; i < total; i++)
+			{
+				var game = new Game(GameConfig());
+				game.StartGame();
+
+				game.Process(ChooseTask.Mulligan(game.Player1, new List<int> { }));
+				game.Process(ChooseTask.Mulligan(game.Player2, new List<int> { }));
+
+				game.MainReady();
+
+				while (game.State != State.COMPLETE)
+				{
+					List<PlayerTask> options = game.CurrentPlayer.Options();
+					PlayerTask option = options[Rnd.Next(options.Count)];
+					//Console.WriteLine(option.FullPrint());
+					game.Process(option);
+				}
+				turns += game.Turn;
+				if (game.Player1.PlayState == PlayState.WON)
+					wins[0]++;
+				if (game.Player2.PlayState == PlayState.WON)
+					wins[1]++;
+			}
+			watch.Stop();
+
+			Console.WriteLine($"{total} games with {turns} turns took {watch.ElapsedMilliseconds} ms => " +
+							  $"Avg. {watch.ElapsedMilliseconds / (float) total} per game " +
+							  $"and {total / (float) watch.ElapsedMilliseconds * 1000.0} games per second " +
+							  $"and {watch.ElapsedMilliseconds / (float) (total * turns)} per turn!");
+			Console.WriteLine($"{turns / (float) total} turns per game!");
+			Console.WriteLine($"playerA {wins[0] * 100.0 / total}% vs. playerB {wins[1] * 100.0 / total}%!");
+		}
+	}
+}
